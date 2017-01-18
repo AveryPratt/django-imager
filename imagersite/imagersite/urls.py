ac66f525bde4f019151bdd27cdcab3821271439f
Avery Pratt <apratt91@gmail.com>
2017-01-17 17:22:59 -0800

    changing branches

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lending_library.views import home_view
+from imager_profile.views import home_view
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
