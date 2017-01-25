c88d504286e473b19fedc071186ee55b49e08980
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-24 19:14:20 -0800

    adding album and photo gallery and detail views, templates, and urls.

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -25,8 +25,21 @@ def photo_gallery_view(request):
         request, "imager_images/photo_gallery.html", {"photos": photos})
 
 
+def photo_detail_view(request, id):
+    """Displays a single photo when clicked on by user."""
+    photo = Photos.objects.get(id=id)
+    return render(request, "imager_images/photo_detail.html", {"photo": photo})
+
+
 def album_gallery_view(request):
     """Display all public albums by all users."""
     albums = Albums.objects.all().filter(published='PU')
     return render(
         request, "imager_images/album_gallery.html", {"albums": albums})
+
+
+def album_detail_view(request, id):
+    """Displays a single album when clicked on by user."""
+    photos = Photos.objects.filter(album__id=id)
+    return render(
+        request, "imager_images/album_detail.html", {"photos": photos})
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from imager_profile.views import home_view, user_profile_view, profile_view
-from imager_images.views import library_view, photo_gallery_view, album_gallery_view
+from imager_images.views import library_view, photo_gallery_view, album_gallery_view, photo_detail_view, album_detail_view
 # import imager_images.urls as photo_urls
 
 
@@ -31,7 +31,9 @@ urlpatterns = [
     url(r'^profile/$', user_profile_view, name='profile'),
     url(r'^images/library/$', library_view, name='library'),
     url(r'^images/photos/$', photo_gallery_view, name='photo_gallery'),
+    url(r'^images/photos/(?P<id>\d+)', photo_detail_view, name='photo_detail'),
     url(r'^images/albums/$', album_gallery_view, name='album_gallery'),
+    url(r'^images/albums/(?P<id>\d+)', album_detail_view, name='album_detail'),
     url(r'^profile/(?P<username>\w+)', profile_view, name="profile"),
     url(r'^registration/', include('registration.backends.hmac.urls')),
 ]
