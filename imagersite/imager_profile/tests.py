824eb8a3e93e78845bb8b6ed9689a90eb3e6e1da
Avery Pratt <apratt91@gmail.com>
2017-01-31 15:45:33 -0800

    changed one line

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -97,7 +97,7 @@ class ProfileFrontEndTests(TestCase):
     def test_home_route_context_foo(self):
         """Test that home route has the right context info."""
         response = self.client.get("/")
-        self.assertTrue(response.context["photo"] and response.context["photo_url"])
+        self.assertTrue(not response.context["photo"] and response.context["photo_url"])
 
     def test_home_route_uses_right_templates(self):
         """Check that home page is using the right templates."""
