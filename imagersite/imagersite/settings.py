d0fb1cb9e83ca12614bc64806018236b549211fa
Avery Pratt <apratt91@gmail.com>
2017-02-02 15:46:08 -0800

    turned debug off

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get("DEBUG", "True"))
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", "True"))
 
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", ""), 'localhost']
 
