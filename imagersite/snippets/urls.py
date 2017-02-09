a90346c9d23f5e348b6ba94101e2a1cc6095bcfe
Avery Pratt <apratt91@gmail.com>
2017-02-09 14:55:35 -0800

    made api views class-based

--- imagersite/snippets/urls.py
+++ imagersite/snippets/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.image_list),
-    url(r'^snippets/(?P<pk>[0-9]+)$', views.image_detail),
+    url(r'^snippets/$', views.ImageList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.ImageDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- imagersite/snippets/views.py
+++ imagersite/snippets/views.py
@@ -1,21 +1,21 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from imager_images.models import Photos
 from snippets.serializers import PhotoSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def image_list(request, format=None):
+class ImageList(APIView):
     """
     List all images, or create a new image.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         images = Photos.objects.all()
         serializer = PhotoSerializer(images, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = PhotoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -23,27 +23,74 @@ def image_list(request, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def image_detail(request, pk, format=None):
+class ImageDetail(APIView):
     """
     Retrieve, update or delete a image instance.
     """
-    try:
-        image = Photos.objects.get(pk=pk)
-    except Photos.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Photos.objects.get(pk=pk)
+        except Photos.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        image = self.get_object(pk)
         serializer = PhotoSerializer(image)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        image = self.get_object(pk)
         serializer = PhotoSerializer(image, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        image = self.get_object(pk)
         image.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET', 'POST'])
+# def image_list(request, format=None):
+#     """
+#     List all images, or create a new image.
+#     """
+#     if request.method == 'GET':
+#         images = Photos.objects.all()
+#         serializer = PhotoSerializer(images, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = PhotoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def image_detail(request, pk, format=None):
+#     """
+#     Retrieve, update or delete a image instance.
+#     """
+#     try:
+#         image = Photos.objects.get(pk=pk)
+#     except Photos.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = PhotoSerializer(image)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = PhotoSerializer(image, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         image.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
